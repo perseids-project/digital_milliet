d299ed83533815e6c38a700b0aad0b0fddb43790
Thibault Clérice <leponteineptique@gmail.com>
2017-05-18 10:04:33 +0200

    Normalized access to Author to offer more flexibility on database naming

--- digital_milliet/lib/author_builder.py
+++ digital_milliet/lib/author_builder.py
@@ -6,11 +6,12 @@ import re
 from MyCapytain.common.reference import URN
 from bson.objectid import ObjectId
 
+
 class AuthorBuilder(object):
     """ Provides methods for building new Author records in the database
     """
 
-    def __init__(self, db=None, catalog=None):
+    def __init__(self, db=None, catalog=None, collection_name="annotation"):
         """ Constructor
 
         :param db: Mongo Db Handle
@@ -20,6 +21,38 @@ class AuthorBuilder(object):
         """
         self.mongo = db
         self.catalog = catalog
+        self.__collection_name__ = collection_name
+
+    @property
+    def collection(self):
+        """ Quick access to Mongo collection
+        """
+        return getattr(self.mongo.db, self.__collection_name__)
+
+    def get_author(self, cts_id):
+        """ Retrieve an author record by CTS ID
+
+        :param cts_id: CTS Identifier
+        :return: Author Record
+        """
+        return self.collection.find_one({"cts_id": cts_id})
+
+    def get_author_by_mongoId(self, _id):
+        """ Retrieve an author record by Mongo Id
+
+        :param _id: Mongo Unique Identifier
+        :return: Author Record
+        """
+        return self.collection.find_one({"_id": _id})
+
+    def update_author(self, cts_id, author_record):
+        """ Update author identified by CTS_ID
+
+        :param cts_id: CTS Identifier
+        :param author_record: Updated Author Record
+        :return: Result of update
+        """
+        return self.collection.update({'cts_id': cts_id}, author_record)
 
     def author_db_build(self, data_dict):
         """ Adds or Updates Author Records in the Annotation Database
@@ -44,7 +77,7 @@ class AuthorBuilder(object):
             auth_id = urn_parts[0]
             work_id = ':'.join(t_parts[0:3]) + ':' + '.'.join(urn_parts[0:2])
 
-            author = self.mongo.db.annotation.find_one({"cts_id": auth_id})
+            author = self.get_author(auth_id)
             if author is None:
                 response_dict = self.catalog.lookup_author(auth_id)
                 for resp in response_dict:
@@ -65,7 +98,7 @@ class AuthorBuilder(object):
                         if len(exists) == 0:
                             work['millnums'].append([millnum, pasg])
 
-                self.mongo.db.annotation.update({'_id': author['_id']}, author)
+                self.update_author(author['cts_id'], author)
             else:
                 print("Unable to get catalog info for " + target)
         except TypeError as err:
@@ -81,7 +114,7 @@ class AuthorBuilder(object):
             print("Invalid data for author build")
             pass
 
-    def make_author(self,resp):
+    def make_author(self, resp):
         """ Make an Author db record from a catalog record and insert it in the database
 
         :param resp: the response from teh catalog lookup
@@ -94,11 +127,11 @@ class AuthorBuilder(object):
         author['name'] = resp['authority_name']
         author['cts_id'] = resp['canonical_id']
         author['works'] = []
-        a_id = self.mongo.db.annotation.insert(author)
-        new_auth = self.mongo.db.annotation.find_one({'_id': a_id})
+        a_id = self.collection.insert(author)
+        new_auth = self.get_author_by_mongoId(a_id)
         return new_auth
 
-    def make_work(self,work_id, millnum, pasg):
+    def make_work(self, work_id, millnum, pasg):
         """ Make a work record from a catalog record
 
         :param work_id: the CTS URN of a work
@@ -121,7 +154,7 @@ class AuthorBuilder(object):
                 work['millnums'] = l
         return work
 
-    def process_comm(self,comm_list):
+    def process_comm(self, comm_list):
         """ Extract a sorted list of milliet numbers from a set of commentary annotations
 
         :param comm_list: set of commentary annotations
@@ -132,7 +165,7 @@ class AuthorBuilder(object):
         """
         millnum_list = []
         convert = lambda text: int(text) if text.isdigit() else text
-        alphanum_key = lambda key: [ convert(re.split('([A-Za-z]+)', key)[0]) ]
+        alphanum_key = lambda key: [convert(re.split('([A-Za-z]+)', key)[0])]
         for row in comm_list:
             try:
                 cite_urn = str(row['commentary'][0]['hasBody']['@id'])
@@ -143,4 +176,4 @@ class AuthorBuilder(object):
                     pass
             except:
                 pass
-        return sorted(millnum_list,key=alphanum_key)
+        return sorted(millnum_list, key=alphanum_key)
